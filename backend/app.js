cd9bc7c7b45fd1fbc5e5e0ec2db9d27aa9dd6922
Maicol Jimenez <maicol_jimenez82121@elpoli.edu.co>
2026-06-30 02:16:01 -0500

    fix: Arreglé la carga dinámica de las rutas y agregé los primeros pasos para la conección a la  base de datos mysql

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,19 @@
 import Server from './src/server/express.server.js';
 
-const server = new Server();
-server.listen();
+async function main() {
+    try {
+        const server = new Server();
+        await server.start();
+    } catch (error) {
+        process.exit(1);
+    }
+}
+
+// Manejo de errores e inicio del servidor
+main().catch(
+    (error) => {
+        console.error('Error fatal:', error);
+        process.exit(1);
+    }
+);
+
